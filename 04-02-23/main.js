3313a559c8dd1263c9c9c570a4409288fd53e1c8
Sardor <sardoribodov07@gmail.com>
2023-02-04 15:47:52 +0500

    4th February challenges

---
+++ 04-02-23/main.js
@@ -0,0 +1,17 @@
+// isPalindrome => 121, 232, ...
+
+// let isPalindrome = (x) => {
+// 	if(x > 0 && x < 10) {
+// 		return true;
+// 	}
+// 	else {
+// 		let palindrome = true;
+// 		arr = x.toString().split("");
+// 		for(let i = 0, j = arr.length - 1; i <= Math.ceil(arr.length / 2); i++, j--) {
+// 			if(arr[i] !== arr[j]) palindrome = false;
+// 		}
+// 		return palindrome;
+// 	}
+// };
+
+
