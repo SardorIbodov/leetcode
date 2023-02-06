d2574c71b297ebe5cbfd19381a0c9ef45b52d4cc
Sardor <sardoribodov07@gmail.com>
2023-02-06 15:23:05 +0500

    6th February challenges

---
+++ 06-02-23/index.js
@@ -0,0 +1,11 @@
+// Shuffle the array
+// [1,2,3,4,4,3,2,1] => [1,4,2,3,3,2,4,1], n = 4
+// [1,1,2,2] => [1,2,1,2], n = 2
+// const shuffle = (nums, n) => {
+// 	result = [];
+// 	for(let i = 0, j = n; i < n, j < nums.length; i++, j++) {
+// 		result.push(nums[i], nums[j]);
+// 	}
+// 	return result;
+// };
+
